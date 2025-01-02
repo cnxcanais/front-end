88bb118adae060e28d0e81d1170641d979d68a03
Eric Kunzel <kunzel.eric@gmail.com>
2025-01-02 13:36:21 -0300

    style: included custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,20 @@ export default {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blue-100': '#cecfd1',
+        'blue-200': '#9fa2a6',
+        'blue-300': '#72777d',
+        'blue-400': '#45505a',
+        'blue-500': '#072a3c',
+        'yellow-100': '#c2a360',
+        'yellow-200': '#8a7444',
+        'yellow-300': '#453a22',
+        'red-error': '#F75A68',
+        'green-success': '#00B37E',
+      },
+    },
   },
   plugins: [],
 } satisfies Config
