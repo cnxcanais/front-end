040b650e95c2cb026aa833a0020080ffeb41b102
Eric Kunzel <kunzel.eric@gmail.com>
2025-01-15 11:06:15 -0300

    config: configured prettier and vscode formatting and installed packages + removed mui packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,41 +1,34 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 export default {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./src/app/(public)/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/(authenticated)/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/modules/**/*.{js,ts,jsx,tsx,mdx}"
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
         black: {
-          100: '#1a1a1a',  
-          200: '#0d0d0d',
+          100: "#1a1a1a",
+          200: "#0d0d0d",
         },
         blue: {
-          100: '#cecfd1',
-          200: '#9fa2a6',
-          300: '#72777d',
-          400: '#45505a',
-          500: '#072a3c',
+          100: "#cecfd1",
+          200: "#9fa2a6",
+          300: "#72777d",
+          400: "#45505a",
+          500: "#072a3c",
         },
         yellow: {
-          100: '#c2a360',
-          200: '#8a7444',
-          300: '#453a22',
+          100: "#c2a360",
+          200: "#8a7444",
+          300: "#453a22",
         },
         beige: {
-          100: '#e0ccbe',
+          100: "#e0ccbe",
         },
         white: {
-          100: '#ffffff',
+          100: "#ffffff",
         },
-        error: '#F75A68',
-        success: '#00B37E'
+        error: "#F75A68",
+        success: "#00B37E",
       },
     },
   },
