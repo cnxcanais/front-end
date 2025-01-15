29c1ce7055f5fc5c52bb0ce09861e35675695314
Eric Kunzel <kunzel.eric@gmail.com>
2025-01-15 11:22:20 -0300

    fix: created page title component and removed page header

--- src/app/core/components/page-header/index.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export function PageHeader({ headName }: { headName: string }) {
-  return (
-    <div className='mt-3 mb-3 text-xl'>
-      <h1>{headName}</h1>
-    </div>
-  );
-}
---
+++ src/core/components/PageTitle.tsx
@@ -0,0 +1,13 @@
+import { ComponentProps } from "react"
+
+export type PageTitleProps = ComponentProps<"h1"> & {
+  content: string
+}
+
+export function PageTitle({ content, ...props }: PageTitleProps) {
+  return (
+    <div className="my-3 text-2xl">
+      <h1 {...props}>{content}</h1>
+    </div>
+  )
+}
