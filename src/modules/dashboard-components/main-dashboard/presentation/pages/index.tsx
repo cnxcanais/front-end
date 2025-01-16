841615f87044e85f0f7f2b06f0b521f41ce73802
Allison Ribeiro <dev.allisonribeiro@gmail.com>
2025-01-16 09:16:11 -0300

    include mock dashboard

--- src/modules/dashboard-components/main-dashboard/presentation/pages/index.tsx
+++ src/modules/dashboard-components/main-dashboard/presentation/pages/index.tsx
@@ -1,7 +1,16 @@
+import { PageTitle } from "@/core/components/PageTitle"
+import Image from "next/image"
+
 export function MainDashboard() {
   return (
-    <div>
-      <h1>Under Construction</h1>
-    </div>
+    <>
+      <PageTitle content="Dashboard" />
+      <Image
+        src="/images/dashboardMock.svg"
+        alt="dashboard"
+        width={1000}
+        height={800}
+      />
+    </>
   )
 }
