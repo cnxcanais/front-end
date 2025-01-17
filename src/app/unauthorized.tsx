4a1ab9094885a41ddc94fda5d083d3539d99dec7
Allison Ribeiro <dev.allisonribeiro@gmail.com>
2025-01-17 13:38:19 -0300

    create loading screen

--- src/app/unauthorized.tsx
+++ src/app/unauthorized.tsx
@@ -7,7 +7,7 @@ export default function Unauthorized() {
   return (
     <>
       <Sidebar />
-      <div className="grid min-h-full grid-cols-1 grid-rows-[1fr_auto_1fr] bg-white lg:grid-cols-[max(50%,36rem)_1fr]">
+      <div className="grid min-h-screen grid-cols-1 grid-rows-[1fr_auto_1fr] bg-white lg:grid-cols-[max(50%,36rem)_1fr]">
         <header className="mx-auto w-full max-w-7xl px-6 pt-6 sm:pt-10 lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:px-8">
           <a href="#">
             <span className="sr-only">Piaseg</span>
@@ -18,7 +18,7 @@ export default function Unauthorized() {
             />
           </a>
         </header>
-        <main className="mx-auto w-full max-w-7xl px-6 py-24 sm:py-32 lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:px-8">
+        <main className="mx-full w-full max-w-7xl px-6 py-24 sm:py-32 lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:px-8">
           <div className="max-w-lg">
             <p className="text-base/8 font-semibold text-indigo-600">401</p>
             <h1 className="mt-4 text-pretty text-5xl font-semibold tracking-tight text-gray-900 sm:text-6xl">
@@ -44,14 +44,12 @@ export default function Unauthorized() {
         <footer className="self-end lg:col-span-2 lg:col-start-1 lg:row-start-3">
           <div className="border-t border-gray-100 bg-gray-50 py-10">
             <nav className="mx-auto flex w-full max-w-7xl items-center gap-x-4 px-6 text-sm/7 text-gray-600 lg:px-8">
-              <a href="#">Contact support</a>
               <svg
                 viewBox="0 0 2 2"
                 aria-hidden="true"
                 className="size-0.5 fill-gray-300">
                 <circle r={1} cx={1} cy={1} />
               </svg>
-              <a href="#">Status</a>
             </nav>
           </div>
         </footer>
--- src/core/components/Sidebar/options.ts
+++ src/core/components/Sidebar/options.ts
@@ -61,7 +61,7 @@ export const sidebar_options: SidebarOptionProps[] = [
   },
   {
     name: "Grupo de Receitas",
-    href: "#",
+    href: "/grupo-receitas",
     Icon: Coins,
     group: "Cadastro",
   },
