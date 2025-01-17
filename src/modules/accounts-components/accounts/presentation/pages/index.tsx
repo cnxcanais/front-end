e725314e8fded15c183ff741f8761b925d576912
Eric Kunzel <kunzel.eric@gmail.com>
2025-01-17 17:38:37 -0300

    fix: removed button from accounts page

--- src/modules/accounts-components/accounts/presentation/pages/index.tsx
+++ src/modules/accounts-components/accounts/presentation/pages/index.tsx
@@ -5,15 +5,20 @@ import { Input } from "@/core/components/Input"
 import { PageTitle } from "@/core/components/PageTitle"
 import { AccountsTable } from "@/modules/accounts-components/accounts/presentation/components/AccountsTable"
 import { FileXls } from "@phosphor-icons/react/dist/ssr"
+import { useRouter } from "next/navigation"
 
 export function AccountsPage() {
+  const { push } = useRouter()
+
   return (
     <main className="flex max-w-[1200px] flex-col">
       <PageTitle content="Contas" />
       <div className="mt-8 flex items-center justify-between">
         <div className="flex gap-4">
           <Input placeholder="Pesquisar" variant="secondary" />
-          <Button variant="secondary">Cadastrar</Button>
+          <Button onClick={() => push("/accounts/create")} variant="secondary">
+            Cadastrar
+          </Button>
         </div>
         <Button className="flex items-center gap-1" variant="secondary">
           <FileXls size={22} />
@@ -21,9 +26,6 @@ export function AccountsPage() {
         </Button>
       </div>
       <AccountsTable />
-      <div>
-        <Button variant="secondary">Salvar</Button>
-      </div>
     </main>
   )
 }
