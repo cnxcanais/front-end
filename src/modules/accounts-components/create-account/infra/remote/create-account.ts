d7aaaafcdfb8fa012a4066ad9ac3c2f4f8761d2e
Eric Kunzel <kunzel.eric@gmail.com>
2025-01-19 19:27:54 -0300

    refactor: separated form component from create account page

--- src/modules/accounts-components/create-account/infra/remote/create-account.ts
+++ src/modules/accounts-components/create-account/infra/remote/create-account.ts
@@ -4,7 +4,7 @@ import { api } from "@/lib/axios"
 export async function createAccount({ name }: Account.CreateRequest) {
   try {
     const { data } = await api.post("/accounts", { name })
-    console.log(data)
+    return data.message
   } catch (error) {
     console.error(error)
   }
