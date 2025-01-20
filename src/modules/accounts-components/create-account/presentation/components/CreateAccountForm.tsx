1935530f81e8e3ff9fc5a15f590879f1a92bc1e6
Eric Kunzel <kunzel.eric@gmail.com>
2025-01-20 10:55:37 -0300

    refactor: adjusted create account form and created schema

--- src/modules/accounts-components/create-account/presentation/components/CreateAccountForm.tsx
+++ src/modules/accounts-components/create-account/presentation/components/CreateAccountForm.tsx
@@ -1,32 +1,61 @@
 "use client"
 
+import { Account } from "@/@types/accounts"
 import { Button } from "@/core/components/Button"
 import * as Input from "@/core/components/Input"
-import { PageTitle } from "@/core/components/PageTitle"
+import { createAccount } from "@/modules/accounts-components/create-account/infra/remote/create-account"
+import {
+  CreateAccountSchema,
+  createAccountSchema,
+} from "@/modules/accounts-components/create-account/presentation/validation/schema"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { useRouter } from "next/navigation"
+import { useForm } from "react-hook-form"
+import { toast } from "sonner"
 
 export function CreateAccountForm() {
   const { push } = useRouter()
 
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<CreateAccountSchema>({
+    resolver: zodResolver(createAccountSchema),
+  })
+
+  async function onSubmit(data: Account.CreateRequest) {
+    try {
+      const response = await createAccount(data)
+      toast.success(response.message)
+      setTimeout(() => push("/accounts"), 2000)
+    } catch (error) {
+      toast.error("Erro ao criar conta: " + error)
+    }
+  }
+
   return (
-    <main>
-      <PageTitle content="Criar Conta" />
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mt-8 flex max-w-96 flex-col gap-2">
         <label className="text-lg" htmlFor="name">
           Nome
         </label>
         <Input.Root>
-          <Input.Control type="text" />
+          <Input.Control {...register("name")} type="text" />
         </Input.Root>
       </div>
       <div className="mt-6 flex gap-4">
-        <Button onClick={() => {}} variant="secondary">
+        <Button type="submit" disabled={isSubmitting} variant="secondary">
           Salvar
         </Button>
-        <Button onClick={() => push("/accounts")} variant="tertiary">
+        <Button
+          type="button"
+          disabled={isSubmitting}
+          onClick={() => push("/accounts")}
+          variant="tertiary">
           Voltar
         </Button>
       </div>
-    </main>
+    </form>
   )
 }
