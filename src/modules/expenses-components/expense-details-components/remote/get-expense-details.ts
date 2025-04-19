6b9636875ff9dd652031d0292287c20116267b1f
Allison Ribeiro <dev.allisonribeiro@gmail.com>
2025-04-19 16:30:44 -0300

    adjust getExpenseDetails to make time UTC

--- src/modules/expenses-components/expense-details-components/remote/get-expense-details.ts
+++ src/modules/expenses-components/expense-details-components/remote/get-expense-details.ts
@@ -6,6 +6,17 @@ export async function getExpenseDetails(
   queryParams: ExpenseDetails.GetRequestParams
 ) {
   try {
+    if (queryParams?.start_date) {
+      const startDate = new Date(queryParams.start_date)
+      startDate.setUTCHours(0, 0, 0, 0)
+      queryParams.start_date = startDate.toDateString()
+    }
+
+    if (queryParams?.end_date) {
+      const endDate = new Date(queryParams.end_date)
+      endDate.setUTCHours(0, 0, 0, 0)
+      queryParams.end_date = endDate.toDateString()
+    }
     const { data } = await api.get<ExpenseDetails.GetResponse>(
       `/expense-details/account/${account_id}`,
       {
