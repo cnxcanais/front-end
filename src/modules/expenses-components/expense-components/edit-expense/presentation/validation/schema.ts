5fca437c890912f1a3d482e2ebe0a33a525cc0e4
Allison Ribeiro <dev.allisonribeiro@gmail.com>
2025-05-06 20:38:54 -0300

    fix percentage validation for expenses

--- src/modules/expenses-components/expense-components/edit-expense/presentation/validation/schema.ts
+++ src/modules/expenses-components/expense-components/edit-expense/presentation/validation/schema.ts
@@ -26,7 +26,7 @@ export const editExpenseSchema = z.object({
   description: z.string().nonempty("Descrição é obrigatória"),
   document: z.string().nonempty("Documento é obrigatório"),
   expense_group_id: z.string().nonempty("Grupo é obrigatório"),
-  expense_percentage: z.coerce.number().min(1, "Porcentagem é obrigatória"),
+  expense_percentage: z.coerce.number().min(0, "Porcentagem é obrigatória"),
   supplier_id: z.string().nonempty("Fornecedor é obrigatório"),
   organization_id: z.string().nonempty("Organização é obrigatória"),
   is_operational: z.boolean({ required_error: "Obrigatório!" }),
