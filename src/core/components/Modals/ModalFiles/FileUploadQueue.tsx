6fac78637d0edc92562ea2976fe08ab7265fb16f
ribeiroAllison <dev.allisonribeiro@gmail.com>
2025-12-07 07:38:56 -0300

    update next version

--- src/core/components/Modals/ModalFiles/FileUploadQueue.tsx
+++ src/core/components/Modals/ModalFiles/FileUploadQueue.tsx
@@ -37,7 +37,9 @@ export function FileUploadQueue({
       refetch()
     } catch (error) {
       console.info("Erro ao fazer upload:", error)
-      toast.error(`Erro ao fazer upload de arquivo(s): ${error}`)
+      toast.error(
+        `Erro ao fazer upload de arquivo(s): ${error.response?.data?.message}`
+      )
     }
   }
 
