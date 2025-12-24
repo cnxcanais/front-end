dcf507fef5523bd41fb1b0f4d3ee58fb3a7cbb2b
ribeiroAllison <dev.allisonribeiro@gmail.com>
2025-12-24 17:19:08 -0300

    edit and create proposta restricted

--- src/app/(authenticated)/propostas/create/page.tsx
+++ src/app/(authenticated)/propostas/create/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 
 import { PageTitle } from "@/core/components/PageTitle"
+import { useProfileAccess } from "@/modules/perfis-components/perfis/infra/hooks/use-profile-access"
 import { PropostaForm } from "@/modules/propostas-components/propostas/presentation/components/PropostaForm"
 import { Suspense } from "react"
 
 export default function CreatePropostaPage() {
+  useProfileAccess(process.env.NEXT_PUBLIC_ADM_ID!)
   return (
     <>
       <PageTitle content="Cadastrar Proposta" />
--- src/app/(authenticated)/propostas/edit/[id]/page.tsx
+++ src/app/(authenticated)/propostas/edit/[id]/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 
 import { PageTitle } from "@/core/components/PageTitle"
+import { useProfileAccess } from "@/modules/perfis-components/perfis/infra/hooks/use-profile-access"
 import { usePropostaByIdQuery } from "@/modules/propostas-components/propostas/infra/hooks/use-proposta-by-id-query"
 import { PropostaForm } from "@/modules/propostas-components/propostas/presentation/components/PropostaForm"
 import { useParams } from "next/navigation"
 
 export default function EditPropostaPage() {
+  useProfileAccess(process.env.NEXT_PUBLIC_ADM_ID!)
   const params = useParams()
   const id = params.id as string
   const { data: proposta, isLoading, refetch } = usePropostaByIdQuery(id)
@@ -15,7 +17,9 @@ export default function EditPropostaPage() {
   return (
     <>
       <PageTitle content="Editar Proposta" />
-      {proposta && <PropostaForm proposta={proposta} isEdit refetchProposta={refetch} />}
+      {proposta && (
+        <PropostaForm proposta={proposta} isEdit refetchProposta={refetch} />
+      )}
     </>
   )
 }
