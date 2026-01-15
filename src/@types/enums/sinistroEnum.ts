a3a78e2cec2e850e76486a04878370f847629514
ribeiroAllison <dev.allisonribeiro@gmail.com>
2026-01-15 12:28:10 -0300

    type sinistro

---
+++ src/@types/enums/sinistroEnum.ts
@@ -0,0 +1,9 @@
+export enum SinistroStatusEnum {
+  NOVO_SINISTRO = "novo_sinistro",
+  EM_ANALISE = "em_analise",
+  EM_REGULACAO = "em_regulacao",
+  APROVADO = "aprovado",
+  REPROVADO = "reprovado",
+  PAGAMENTO = "pagamento",
+  ENCERRADO = "encerrado",
+}
---
+++ src/@types/sinistro.ts
@@ -0,0 +1,70 @@
+import { SinistroStatusEnum } from "./enums/sinistroEnum"
+
+export namespace Sinistro {
+  export type Type = {
+    id: string
+    numeroSinistro: string
+    apoliceId: string
+    tipoSinistroId: string
+    emailSegurado: string
+    dataHoraOcorrido: string
+    descricaoOcorrido: string
+    status: SinistroStatusEnum
+    prestadora: string
+    andamento: string
+    dataUltimaTratativa: string
+    responsavelUsuarioId: string
+    valorEstimado: number
+    valorAprovado: number
+    formaPagamento: string
+    dataPagamento: string
+    motivoReprovacao: string
+    justificativaReprovacao: string
+    createdAt: string
+    updatedAt: string
+    createdBy: string
+    updatedBy: string
+  }
+
+  export type GetResponse = {
+    items: Type[]
+    total: number
+    page: number
+    limit: number
+    totalPages: number
+  }
+
+  export type PatchBody = Partial<
+    Omit<
+      Type,
+      | "id"
+      | "numeroSinistro"
+      | "apoliceId"
+      | "tipoSinistroId"
+      | "status"
+      | "createdAt"
+      | "updatedAt"
+      | "createdBy"
+      | "updatedBy"
+    >
+  >
+
+  export type PostStatusBody = {
+    statusNovo?: SinistroStatusEnum
+    observacao?: string
+    andamento?: string
+    valorEstimado?: number
+    valorAprovado?: number
+    formaPagamento?: string
+    dataPagamento?: string
+    motivoReprovacao?: string
+    justificativaReprovacao?: string
+    prestadora?: string
+    dataUltimaTratativa?: string
+    responsavelUsuarioId?: string
+  }
+
+  export type PostObservacaoBody = {
+    observacao: string
+  }
+}
