ab1537e6ee9db788405f487125c4fb0c12541aa0
ribeiroAllison <dev.allisonribeiro@gmail.com>
2026-01-18 20:33:55 -0300

    authorize sinistro to non admin

--- src/app/(authenticated)/sinistros/page.tsx
+++ src/app/(authenticated)/sinistros/page.tsx
@@ -1,11 +1,9 @@
 "use client"
 
 import { PageTitle } from "@/core/components/PageTitle"
-import { useProfileAccess } from "@/modules/perfis-components/perfis/infra/hooks/use-profile-access"
 import { SinistroKanbam } from "@/modules/sinistros-components/sinistro/presentation/components/SinistroKanbam"
 
 export default function SuppliersRender() {
-  useProfileAccess(process.env.NEXT_PUBLIC_ADM_ID!)
   return (
     <main className="flex w-full flex-col">
       <PageTitle content="Sinistros" />
