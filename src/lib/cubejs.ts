460b4f2b0722c6f71049ebc6405cc1dcfcdd617b
ribeiroAllison <dev.allisonribeiro@gmail.com>
2026-01-25 14:05:06 -0300

    change env

--- src/lib/cubejs.ts
+++ src/lib/cubejs.ts
@@ -1,9 +1,7 @@
 import cubejs from "@cubejs-client/core"
 
 const cubeApi = cubejs(process.env.NEXT_PUBLIC_CUBEJS_TOKEN || "", {
-  apiUrl:
-    process.env.NEXT_PUBLIC_CUBEJS_API_URL ||
-    "http://localhost:4000/cubejs-api/v1",
+  apiUrl: process.env.NEXT_PUBLIC_CUBEJS_API_URL,
 })
 
 export default cubeApi
