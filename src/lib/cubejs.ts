9b5ebcd1891a4b7a36617741bc8d9fc6a0687cfb
ribeiroAllison <dev.allisonribeiro@gmail.com>
2026-02-16 17:56:06 -0300

    include token on cube requests

--- src/lib/cubejs.ts
+++ src/lib/cubejs.ts
@@ -1,9 +1,17 @@
 import cubejs from "@cubejs-client/core"
+import { getCookie } from "./cookies"
 
 const apiUrl = process.env.NEXT_PUBLIC_CUBEJS_API_URL
 
+function getUpdatedToken() {
+  return getCookie("token")
+}
+
 const cubeApi = cubejs(process.env.NEXT_PUBLIC_CUBEJS_TOKEN || "", {
   apiUrl: apiUrl,
+  headers: {
+    Authorization: `Bearer ${getUpdatedToken()}`,
+  },
 })
 
 export default cubeApi
