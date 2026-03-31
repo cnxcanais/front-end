45554a55fe92eafb94ecfd8f08153c149899294c
ribeiroAllison <dev.allisonribeiro@gmail.com>
2026-03-31 19:12:48 -0300

    fix toast error

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -59,7 +59,7 @@ bffApi.interceptors.request.use(
 bffApi.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
+    if (error.response?.status === 401 && !error.config?.url?.includes("/auth/login")) {
       removeCookie("token")
       removeCookie("userId")
       removeCookie("corretoraId")
